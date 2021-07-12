fb565526d93465b9a4d1dfb2812dbe2de3a791a1
Juntai Park <juntai81@gmail.com>
2021-07-12 14:19:11 +0900

    Remove and Ignore dist

--- dist/lib/OkkyList.d.ts
+++
@@ -1,7 +0,0 @@
-import { OptionValues } from 'commander';
-export default class OkkyList {
-    constructor();
-    getList: (optionValues: OptionValues) => Promise<string | undefined>;
-    private parseHtml;
-}
-export declare const okkyList: OkkyList;
